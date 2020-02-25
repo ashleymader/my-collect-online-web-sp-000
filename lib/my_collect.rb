90f11c99241c9b37708515cd1c29f0f7e68fa8ae
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-25 14:33:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,4 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-def my_collect(array) do |name|
-  name.split(" ").first
-end
 
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-def my_collect(collection) do |lang|
-  lang.upcase
+def my_collect(array) 
+  
 end
